f9643f2645038c4a1d9d8d5ed8e77e0a900502c7
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-14 19:22:03 -0600

    Add zsh/bash word auto-completion.

---
+++ completions/ru.bash
@@ -0,0 +1,18 @@
+# shellcheck shell=bash
+
+_ru() {
+    local current wiktionary
+    wiktionary="$HOME/Documents/Zict/wiktionary_ru_all_maxi_2022-08.zim"
+    current="${COMP_WORDS[COMP_CWORD]}"
+
+    COMPREPLY=$(
+        zimsearch "$wiktionary" "$current" |
+        awk '/^score/ { for (i=4; i < NF; ++i) printf $i FS; printf $NF"\n" }'
+    )
+
+    return 0
+}
+
+complete -o nospace -F _ru ru
+
+# vim: tabstop=4 shiftwidth=4 expandtab filetype=sh
--- completions/ru.zsh
+++ completions/ru.zsh
@@ -1,15 +1,17 @@
-#!/usr/bin/bash
+#compdef ru
+# shellcheck disable=2148
 
-_ru()
-{
+_ru() {
     local current wiktionary
     wiktionary="$HOME/Documents/Zict/wiktionary_ru_all_maxi_2022-08.zim"
     current="${COMP_WORDS[COMP_CWORD]}"
-    COMPREPLY=("$(
+
+    COMPREPLY=$(
         zimsearch "$wiktionary" "$current" |
         awk '/^score/ { for (i=4; i < NF; ++i) printf $i FS; printf $NF"\n" }'
-    )")
+    )
+
     return 0
 }
 
-complete -o nospace -F _ru ru
+# vim: tabstop=4 shiftwidth=4 expandtab filetype=sh
