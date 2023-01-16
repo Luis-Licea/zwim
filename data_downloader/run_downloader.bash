27697492d886e420ba2d448c62b187a75be72f07
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-15 20:08:07 -0600

    Move dictionary download functionality to Zict.

--- data_downloader/run_downloader.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=1090
-
-URL_DOWNLOAD_LIST='./urls_to_download.bash'
-[[ -f "$URL_DOWNLOAD_LIST" ]] && source "$URL_DOWNLOAD_LIST" || exit
-
-for url_to_download in "${URLS[@]}"; do
-    wget --directory-prefix ~/Documents/Zict --no-clobber "$url_to_download"
-done
--- data_downloader/urls_to_download.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-URLS=(
-    # The Russian Wiktionary weighs about 2GB.
-    'https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_ru_all_maxi_2022-08.zim'
-    # The English Wiktionary weighs about 7GB.
-    'https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_en_all_maxi_2022-09.zim'
-)
