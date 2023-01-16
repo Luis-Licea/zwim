1ae5462ec08b2db24150a4356d2a1acdf65e478d
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-15 18:58:54 -0600

    Add Zict config file.

---
+++ data_downloader/zict.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# The directory where dictionaries will be downloaded.
+DOWNLOAD_DIR="$HOME/Documents/Zict"
+
+# The URLs from which the dictionaries will be downloaded.
+declare -A WIKTIONARY_DOWNLOAD_URL=(
+    # The Russian Wiktionary weighs about 2GB.
+    [ru]='https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_ru_all_maxi_2022-08.zim'
+    # The English Wiktionary weighs about 7GB.
+    [en]='https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_en_all_maxi_2022-09.zim'
+)
+
+# The paths to the dictionaries.
+declare -A WIKTIONARY_PATH=(
+    [ru]="$DOWNLOAD_DIR/wiktionary_ru_all_maxi_2022-08.zim"
+    [en]="$DOWNLOAD_DIR/wiktionary_en_all_maxi_2022-09.zim"
+)
