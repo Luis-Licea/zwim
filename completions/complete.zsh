661650a2b14e1c7fd8a25eb85732a1ce666abdf6
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-20 18:25:11 -0600

    Add Zsh word-completion.

--- completions/complete.zsh
+++ completions/complete.zsh
@@ -1,15 +1,38 @@
 #compdef zict
-# shellcheck disable=2148
+# shellcheck disable=2034,2296,2148,2154
+
+search() {
+    local -r wiktionary="$HOME/Documents/Zict/wiktionary_ru_all_maxi_2022-08.zim"
+    zimsearch "$wiktionary" "$*" |
+        awk '/^score/ { for (i=4; i < NF; ++i) printf $i FS; printf $NF"\n" }'
+}
 
 _zict() {
-    local current wiktionary
-    wiktionary="$HOME/Documents/Zict/wiktionary_ru_all_maxi_2022-08.zim"
-    current="${COMP_WORDS[COMP_CWORD]}"
+    if ((CURRENT == 2)); then
 
-    COMPREPLY=$(
-        zimsearch "$wiktionary" "$current" |
-            awk '/^score/ { for (i=4; i < NF; ++i) printf $i FS; printf $NF"\n" }'
-    )
+        _arguments : \
+            '(-)'{--help,-h}'[Show help message]' \
+            '(--download -d)'{--download,-d}'[Download the given dictionary]' \
+            '--en[View English prase]' \
+            '--ru[View Russian prase]'
 
-    return 0
+    else
+        local option="${words[2]}"
+        case "$option" in
+        -d | --download)
+            local -a commands=(
+                "en:The English Wiktionary"
+                "ru:The Russian Wiktionary"
+            )
+            _describe -t commands 'zict download' commands
+            ;;
+        --ru)
+            # Convert result into an array.
+            local -a results=("${(f)$(search "${words[@]:2}")}")
+            _describe -t commands 'zict --ru' results
+            ;;
+        esac
+    fi
 }
+
+# vim: ft=sh
