de9aa88df345e19b49b8f2d9dc4862673df9efc3
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-20 19:31:06 -0600

    Add options without double dash prefix.

--- completions/complete.zsh
+++ completions/complete.zsh
@@ -3,12 +3,23 @@
 
 _zict() {
     if ((CURRENT == 2)); then
-        _arguments : \
-            '(-)'{--help,-h}'[Show help message]' \
-            '(--download -d)'{--download,-d}'[Download the given dictionary]' \
-            '(--search -s)'{--search,-s}'[Search similar words in given dictionary]' \
-            '--en[View English prase]' \
-            '--ru[View Russian prase]'
+        if [[ ${words[CURRENT]} = -* ]]; then
+            _arguments : \
+                '(-)'{--help,-h}'[Show help message]' \
+                '(--download -d)'{--download,-d}'[Download the given dictionary]' \
+                '(--search -s)'{--search,-s}'[Search similar words in given dictionary]' \
+                '--en[View English prase]' \
+                '--ru[View Russian prase]'
+        else
+            local -a commands=(
+                'help:Show help message'
+                'download:Download the given dictionary'
+                'search:Search similar words in given dictionary'
+                'en:View English prase'
+                'ru:View Russian prase'
+            )
+            _describe -t commands 'zict' commands
+        fi
     else
         local option="${words[2]}"
         local -a dictionaries=(
@@ -16,19 +27,19 @@ _zict() {
             "ru:The Russian Wiktionary"
         )
         case "$option" in
-        -d | --download)
+        -d | --download | download)
             # Do not make more suggestions: a suggestion has been selected.
             ((CURRENT >= 4)) && return 0
-            _describe -t commands 'zict --download' dictionaries
+            _describe -t commands 'zict download' dictionaries
             ;;
-        -s | --search)
+        -s | --search | search)
             ((CURRENT >= 4)) && return 0
-            _describe -t commands 'zict --search' dictionaries
+            _describe -t commands 'zict search' dictionaries
             ;;
-        --ru | --en)
+        --ru | ru | --en | en)
             # Convert result into an array.
             local -a results=("${(f)$(zict --search "${words[2]#--}" "${words[@]:2}")}")
-            _describe -t commands 'zict --ru' results
+            _describe -t commands 'zict <language>' results
             ;;
         esac
     fi
