b70a3fa7ef16c7c851371f451acef627655825af
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-25 21:28:03 -0600

    Add Spanish Wiktionary.

--- configuration/zict.bash
+++ configuration/zict.bash
@@ -6,12 +6,14 @@ declare -r MY_DOWNLOAD_DIR="$(xdg-user-dir DOCUMENTS)/Zict"
 
 # The URLs from which the dictionaries will be downloaded.
 declare -rA MY_DOWNLOAD_URLS=(
-    # The Russian Wiktionary weighs about 2GB.
-    [ru]="https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_ru_all_maxi_2022-08.zim"
     # The English Wiktionary weighs about 7GB.
     [en]="https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_en_all_maxi_2022-09.zim"
+    # The Spanish Wiktionary weighs about 700MB.
+    [es]="https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_es_all_maxi_2023-01.zim"
     # The Japanese Wiktionary weighs about 400MB.
     [ja]="https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_ja_all_maxi_2022-12.zim"
+    # The Russian Wiktionary weighs about 2GB.
+    [ru]="https://dumps.wikimedia.org/other/kiwix/zim/wiktionary/wiktionary_ru_all_maxi_2022-08.zim"
 )
 
 # The paths to the dictionaries.
@@ -19,11 +21,13 @@ declare -rA MY_ZIM_FILES=(
     [ru]="$MY_DOWNLOAD_DIR/wiktionary_ru_all_maxi_2022-08.zim"
     [en]="$MY_DOWNLOAD_DIR/wiktionary_en_all_maxi_2022-09.zim"
     [ja]="$MY_DOWNLOAD_DIR/wiktionary_ja_all_maxi_2022-12.zim"
+    [es]="$MY_DOWNLOAD_DIR/wiktionary_es_all_maxi_2023-01.zim"
 )
 
-# Not necessary, but improves tab-auto completion suggestions.
+# Not necessary, but improves tab-auto completion messages.
 declare -ra MY_ZIM_HINTS=(
-    "ru:Russian Wiktionary"
     "en:English Wiktionary"
+    "es:Spanish Wiktionary"
     "ja:Japanese Wiktionary"
+    "ru:Russian Wiktionary"
 )
