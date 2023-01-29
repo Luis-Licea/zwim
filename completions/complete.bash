19f626f7ee1208167640819fb5f2a85c32f4f118
Luis David Licea Torres <luislicea@hotmail.com>
2023-01-28 20:35:43 -0600

    Remove duplicate options.

--- completions/complete.bash
+++ completions/complete.bash
@@ -6,57 +6,52 @@ _zict() {
     if ((COMP_CWORD == 1)); then
         if [[ "$current" = -* ]]; then
             options=(
-                -a
-                -d
+                -c
+                -f
                 -h
-                -s
             )
         elif [[ "$current" = --* ]]; then
             options=(
-                --alter
-                --config-file
                 --copy-config
-                --download
+                --find-config
                 --help
-                --search
             )
         else
             options=(
                 alter
                 download
-                help
                 search
             )
-            source "$(zict --config-file)"
+            source "$(zict --find-config)"
             options+=("${!MY_ZIM_FILES[@]}")
         fi
         mapfile -t COMPREPLY < <(compgen -W "${options[*]}" -- "$current")
     else
-        source "$(zict --config-file)"
+        source "$(zict --find-config)"
         local -ra dictionaries=("${!MY_ZIM_FILES[@]}")
         local -r option="${COMP_WORDS[1]}"
         case "$option" in
-        -a | --alter | alter)
+        a | alter)
             # Suggest a dictionary.
             if [[ -z "${COMP_WORDS[2]}" ]]; then
                 mapfile -t COMPREPLY < <(compgen -W "${dictionaries[*]}" -- "$current")
                 return
             fi
             # Convert result into an array.
-            mapfile -t words < <(zict --search "${COMP_WORDS[2]}" "${COMP_WORDS[@]:3}")
+            mapfile -t words < <(zict search "${COMP_WORDS[2]}" "${COMP_WORDS[@]:3}")
             # Replace every space with a backslash and a space.
             # For example: "a short string" becomes "a\ short\ string".
             mapfile -t COMPREPLY < <(compgen -W "${words[*]// /\\ }" -- "$current")
             ;;
-        -d | --download | download | \
-        -s | --search | search)
+        d | download | \
+        s | search)
             # Do not make more suggestions: a suggestion has been selected.
             ((COMP_CWORD >= 3)) && return
             mapfile -t COMPREPLY < <(compgen -W "${dictionaries[*]}" -- "$current")
             ;;
         ru | en)
             # Convert result into an array.
-            mapfile -t words < <(zict --search "$option" "${COMP_WORDS[@]:2}")
+            mapfile -t words < <(zict search "$option" "${COMP_WORDS[@]:2}")
             # Replace every space with a backslash and a space.
             # For example: "a short string" becomes "a\ short\ string".
             mapfile -t COMPREPLY < <(compgen -W "${words[*]// /\\ }" -- "$current")
--- completions/complete.zsh
+++ completions/complete.zsh
@@ -4,50 +4,49 @@
 _zict() {
     if ((CURRENT == 2)); then
         if [[ "${words[CURRENT]}" = -* ]]; then
-            _arguments : \
-                '(-)'{--help,-h}'[Show help message]' \
-                '(--download -d)'{--download,-d}'[Download the given file]' \
-                '(--search -s)'{--search,-s}'[Search similar words]' \
-                '--alter[Preprocess before viewing]' \
-                '--config-file[The path to the config file being used]' \
-                '--copy-config[Copy the default configuration file]'
+            local -a commands=(
+                '(-)'{--help,-h}'[Show help message]'
+                '(--copy-config -c)'{--copy-config,-c}'[Copy default configuration file]'
+                '(--find-config -f)'{--find-config,-f}'[Get path to configuration file]'
+                '(--search -s)'{--search,-s}'[Search similar words]'
+            )
+            _arguments $commands
         else
             local -a commands=(
                 'alter:Preprocess before viewing'
                 'download:Download the given file'
-                'help:Show help message'
                 'search:Search similar words'
             )
-            source "$(zict --config-file)"
+            source "$(zict --find-config)"
             commands+=("${MY_ZIM_HINTS[@]}")
 
             _describe -t commands 'zict' commands
         fi
     else
         local option="${words[2]}"
-        source "$(zict --config-file)"
+        source "$(zict --find-config)"
         local -ra dictionaries=("${MY_ZIM_HINTS[@]}")
 
         case "$option" in
-        -a | --alter | alter)
+        a | alter)
             # Suggest a dictionary.
             if [[ -z "${words[3]}" ]]; then
                 _describe -t commands 'zict <option>' dictionaries
                 return
             fi
             # Convert result into an array.
-            local -a results=("${(f)$(zict --search "${words[3]}" "${words[@]:3}")}")
+            local -a results=("${(f)$(zict search "${words[3]}" "${words[@]:3}")}")
             _describe -t commands 'zict alter <lang> <option>' results
         ;;
-        -d | --download | download | \
-        -s | --search | search)
+        d | download | \
+        s | search)
             # Do not make more suggestions: a suggestion has been selected.
             ((CURRENT >= 4)) && return
             _describe -t commands 'zict <option>' dictionaries
             ;;
         ru | en)
             # Convert result into an array.
-            local -a results=("${(f)$(zict --search "$option" "${words[@]:2}")}")
+            local -a results=("${(f)$(zict search "$option" "${words[@]:2}")}")
             _describe -t commands 'zict <language>' results
             ;;
         esac
