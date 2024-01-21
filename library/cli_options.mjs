6483a158354f1e6ba5dbb1912bbf152bcc89ec18
Luis David Licea Torres <luislicea@hotmail.com>
2024-01-21 00:42:39 -0600

    Add cli options and corresponding unit test

---
+++ library/cli_options.mjs
@@ -0,0 +1,122 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argument = {
+    language: [
+        "language",
+        {
+            type: "string",
+            describe: "The language dictionary to use for the search",
+        },
+    ],
+    words: [
+        "words",
+        {
+            type: "string",
+            describe: "The words to search",
+        },
+    ],
+    path: [
+        "path",
+        {
+            type: "string",
+            describe: "The path where to save the search result",
+        },
+    ],
+};
+
+/**
+ * Return the command-line arguments.
+ *
+ * @param {string[]} [argv=process.argv] The arguments.
+ * @returns {Promise<{_: string[], $0: string, words: string[]?, language:
+ * string?, languages: string[]?, path: string?}>}
+ * */
+export default async function cliOptions(argv = hideBin(process.argv)) {
+    return yargs(argv)
+        .scriptName("zwim")
+        .usage("$0 <command> [arguments]")
+        .command("show <language> <words...>", "Show the language word definition.", (yargs) => {
+            yargs.positional(...argument.language).positional(...argument.words)
+        })
+        .command(
+            "alter <language> <words...>",
+            "Alter and show the search result.",
+            (yargs) => {
+                yargs
+                    .positional(...argument.language)
+                    .positional(...argument.words);
+            },
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "download <languages...>",
+            "The language dictionaries to download.",
+            (yargs) => {
+                yargs.positional("languages", {
+                    type: "string",
+                    describe: "The language dictionaries to download",
+                });
+            },
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "output <path> <language> <words...>",
+            "Save the search result to the given path.",
+            (yargs) => {
+                yargs
+                    .positional(...argument.path)
+                    .positional(...argument.language)
+                    .positional(...argument.words);
+            },
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "output-alter <path> <language> <words...>",
+            "Save the altered search result to the given path.",
+            (yargs) => {
+                yargs
+                    .positional(...argument.path)
+                    .positional(...argument.language)
+                    .positional(...argument.words);
+            },
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "search <language> <words...>",
+            "Search similar words in the given language.",
+            (yargs) => {
+                yargs
+                    .positional(...argument.language)
+                    .positional(...argument.words);
+            },
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "copy-config",
+            "Copy the default configuration file to TODO",
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .command(
+            "find-config",
+            "Return the path to the default configuration file.",
+            function (argv) {
+                console.log("hello", argv.language, "welcome to yargs!");
+            },
+        )
+        .demandCommand()
+        .help()
+        .parse();
+}
---
+++ test/cli_options.test.mjs
@@ -0,0 +1,95 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+
+import cliOptions from "../library/cli_options.mjs";
+
+test("show command", async () => {
+    const expected = {
+        command: "show",
+        language: "english",
+        words: ["the", "quick", "brown", "fox"],
+    };
+    const actual = await cliOptions([
+        expected.command,
+        expected.language,
+        ...expected.words,
+    ]);
+    console.log(actual);
+    assert.deepEqual(actual._[0], expected.command);
+    assert.deepEqual(actual.language, expected.language);
+    assert.deepEqual(actual.words, expected.words);
+});
+
+test("alter command", async () => {
+    const expected = {
+        command: "alter",
+        language: "english",
+        words: ["the", "quick", "brown", "fox"],
+    };
+    const actual = await cliOptions([
+        expected.command,
+        expected.language,
+        ...expected.words,
+    ]);
+    assert.deepEqual(actual._[0], expected.command);
+    assert.deepEqual(actual.language, expected.language);
+    assert.deepEqual(actual.words, expected.words);
+});
+
+test("download command", async () => {
+    const expected = {
+        command: "download",
+        lanaguages: ["english", "spanish", "русский"],
+    };
+    const actual = await cliOptions([expected.command, ...expected.lanaguages]);
+    assert.deepEqual(actual._[0], expected.command);
+    assert.deepEqual(actual.languages, expected.lanaguages);
+});
+
+for (const command of ["output", "output-alter"]) {
+    test(`${command} command`, async () => {
+        const expected = {
+            command: command,
+            path: "/tmp/hello_world.html",
+            language: "english",
+            words: ["hello", "world"],
+        };
+        const actual = await cliOptions([
+            expected.command,
+            expected.path,
+            expected.language,
+            ...expected.words,
+        ]);
+        assert.deepEqual(actual._[0], expected.command);
+        assert.deepEqual(actual.path, expected.path);
+        assert.deepEqual(actual.language, expected.language);
+        assert.deepEqual(actual.words, expected.words);
+    });
+}
+
+test("search command", async () => {
+    const expected = {
+        command: "search",
+        language: "english",
+        words: ["hello", "world"],
+    };
+    const actual = await cliOptions([
+        expected.command,
+        expected.language,
+        ...expected.words,
+    ]);
+    assert.deepEqual(actual._[0], expected.command);
+    assert.deepEqual(actual.language, expected.language);
+    assert.deepEqual(actual.words, expected.words);
+});
+
+for (const command of ["copy-config", "find-config"]) {
+    test(`${command} command`, async () => {
+        const expected = {
+            command: command,
+            language: "english",
+        };
+        const actual = await cliOptions([expected.command]);
+        assert.deepEqual(actual._[0], expected.command);
+    });
+}
