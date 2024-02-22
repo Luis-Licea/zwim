e33bc7b19ccae97923cc3e8afef20c6f77445f6a
Luis David Licea Torres <luislicea@hotmail.com>
2024-02-21 19:05:08 -0600

    Create nix installer

--- installers/nix/derivation.nix
+++ installers/nix/derivation.nix
@@ -1,15 +1,16 @@
 {
   # bash,
-  fcitx5,
+  # fcitx5,
   lib,
-  stdenv,
-  w3m,
+  # stdenv,
+  # w3m,
   wayland,
   # xdg-user-dirs,
-  zim-tools,
+  # zim-tools,
+  buildNpmPackage,
   ...
 }:
-stdenv.mkDerivation rec {
+buildNpmPackage rec {
   pname = "zwim";
   version = "1.0.0";
 
@@ -25,44 +26,8 @@ stdenv.mkDerivation rec {
   #   # rev = "de7afb50a20389079f09a5f43091629861279fdf";
   # };
 
-  outputs = ["out" "man"];
-
-          # --replace 'bash' '${bash}/bin/bash' \
-          # --replace '/usr/bin/env xdg-user-dir' '${xdg-user-dirs}/bin/xdg-user-dir' \
-          # --replace 'conf_default_path="/etc/zwim/$conf_name"' 'conf_default_path="${placeholder "out"}/share/${pname}-${version}/$conf_name"'
-  preConfigure = ''
-    substituteInPlace executable/index.mjs completions/complete.bash \
-          --replace 'fcitx5-remote' '${fcitx5}/bin/fcitx5-remote' \
-          --replace 'w3m' '${w3m}/bin/w3m' \
-          --replace 'zimdump' '${zim-tools}/bin/zimdump' \
-          --replace 'zimsearch' '${zim-tools}/bin/zimsearch'
-
-    substituteInPlace manual/zwim.1 \
-        --replace "/usr/share/zwim/zwim.yml" "${placeholder "out"}/share/${pname}-${version}/zwim.yml"
-
-  '';
-  installPhase = ''
-    # Install license.
-    find . -name LICENSE-GPL3.txt -type f -exec install -Dm644 {} \
-        "$out/share/licenses/$name/LICENSE-GPL3.txt" \;
-
-    # Install shell completion files.
-    find . -name complete.zsh -type f -exec install -Dm644 {} \
-        "$out/share/zsh/site-functions/_$pname" \;
-    find . -name complete.bash -type f -exec install -Dm644 {} \
-        "$out/share/bash-completion/completions/$pname" \;
-
-    # Install manual page.
-    find . -name zwim.1 -type f -exec install -Dm644 {} \
-        "$man/share/man/man1/zwim.1" \;
-
-    # Install configuration file.
-    find . -name zwim.yml -type f -exec install -Dm644 {} \
-        "$out/share/$name/zwim.yml" \;
-
-    # Install executable.
-    install -Dm 755 "$pname" "$out/bin/$pname"
-  '';
+  npmDepsHash = "sha256-Eikp/WUVP4npQqzFZJZYxQtbXMzGrNPTg/wqjHfVzEI=";
+  dontNpmBuild = true;
 
   meta = {
     homepage = "https://github.com/luis-licea/zwim/";
